2048e357ff80b8c02b53ae172800e1ca712d4aca
ZhengChengkai826 <zchengkai826@gmail.com>
2019-04-14 02:46:20 -0700

    fund 2

---
+++ SplitViewCalc/CalcInputButton.swift
@@ -0,0 +1,16 @@
+//
+//  CalcInputButton.swift
+//  SplitViewCalc
+//
+//  Created by iqra on 2019/4/14.
+//  Copyright © 2019 iqra. All rights reserved.
+//
+
+import UIKit
+
+class CalcInputButton: UIButton {
+    var exprDisp: UILabel?
+    
+    
+
+}
